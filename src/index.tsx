40d0fb7df19e7673157af61583b45d2ea8b28385
koko <koko.bozhkov@elementhuman.com>
2022-05-15 18:36:22 +0300

    re-enable strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -13,9 +13,9 @@ if (!container) throw Error('No root container?!')
 const root = ReactDOMClient.createRoot(container)
 
 root.render(
-    // <React.StrictMode>
-    <App />
-    // </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
